16522d598219e044dcc1b146737ba6ff5d47d39f
Balaraju164 <balaraju164570@gmail.com>
2020-10-31 19:48:37 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { mongo, Mongoose } = require('mongoose');
 const connectDB = require('./config/db');
+const path = require('path')
+
 const app = express();
 
 //@connect DB
@@ -10,15 +12,25 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API is running....');
-});
+// app.get('/', (req, res) => {res.send('API is running....');});
 
 //@Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+//serve static assests in production
+if(process.env.NODE_ENV=== 'production')
+{
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+
+}
 
 const PORT = process.env.PORT || 5000;
 
